c17f206861d566507054677dd6ec591217d316c0
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-06 19:03:33 +0000

    adds basic colour scheme

--- public/app.js
+++ public/app.js
@@ -11,27 +11,8 @@ var requestComplete = function(){
     var jsonString = this.responseText;
     var beers = JSON.parse(jsonString);
 
-    createBeerLi(returnNames(beers));
+    // createBeerLi(returnNames(beers));
     createBeerAndImage(beers);
-}
-
-var returnNames = function(beers){
-    namesArray = [];
-    for (beer of beers){
-        namesArray.push(beer.name);
-    }
-    return namesArray
-}
-
-var createBeerLi = function(array){
-    var display = document.getElementById("beer-list");
-
-    for (item of array){
-        var li = document.createElement('li');
-        li.innerText = item;
-        display.appendChild(li);
-
-    }
 }
 
 var createBeerAndImage = function(beers){
@@ -40,14 +21,19 @@ var createBeerAndImage = function(beers){
     for (item of beers){
         var li = document.createElement('li');
         li.innerText = item.name;
-        display.appendChild(li);
+        li.classList = "beer-titles"
+
+        var div = document.createElement('div');
+        div.classList.add('beer-div')
+        div.appendChild(li);
         var img = document.createElement('IMG');
         img.setAttribute('src', item.image_url);
         img.classList.add("beer-image")
-        display.appendChild(img);
+        div.appendChild(img);
+        display.appendChild(div);
     }
-
 }
+
 var app = function(){
     var url = "https://api.punkapi.com/v2/beers"
     makeRequest(url, requestComplete);
