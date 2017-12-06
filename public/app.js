27545d3bbe051009d79bed526d2dfdaaf746a216
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-06 16:07:36 +0000

    adds beer and image as uls

--- public/app.js
+++ public/app.js
@@ -11,28 +11,42 @@ var requestComplete = function(){
     var jsonString = this.responseText;
     var beers = JSON.parse(jsonString);
 
-    createLi(returnNames(beers));
+    createBeerLi(returnNames(beers));
+    createBeerAndImage(beers);
 }
 
 var returnNames = function(beers){
     namesArray = [];
-
     for (beer of beers){
         namesArray.push(beer.name);
     }
-
     return namesArray
 }
 
-var createLi = function(array){
-    console.log(array);
+var createBeerLi = function(array){
     var display = document.getElementById("beer-list");
 
     for (item of array){
         var li = document.createElement('li');
         li.innerText = item;
         display.appendChild(li);
+
     }
+}
+
+var createBeerAndImage = function(beers){
+    var display = document.getElementById("beer-list");
+
+    for (item of beers){
+        var li = document.createElement('li');
+        li.innerText = item.name;
+        display.appendChild(li);
+        var img = document.createElement('IMG');
+        img.setAttribute('src', item.image_url);
+        img.classList.add("beer-image")
+        display.appendChild(img);
+    }
+
 }
 var app = function(){
     var url = "https://api.punkapi.com/v2/beers"
