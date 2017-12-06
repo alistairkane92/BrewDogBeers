282a0a8cb7757144ec2e62ceb32d4244763c0334
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-06 15:38:11 +0000

    creates a list of beer names

--- public/app.js
+++ public/app.js
@@ -7,12 +7,28 @@ var makeRequest = function(url, callback){
 
 var requestComplete = function(){
     if(this.status !== 200) return;
-    console.log("Connected");
+
+    var jsonString = this.responseText;
+    var beers = JSON.parse(jsonString);
+
+    displayBeers(beers);
+}
+
+var displayBeers = function(beers){
+    var display = document.getElementById("beer-list");
+
+    for (beer of beers){
+        var newItem = document.createElement("li");
+        newItem.innerText = beer.name;
+        display.appendChild(newItem);
+    }
 }
 
 var app = function(){
     var url = "https://api.punkapi.com/v2/beers"
     makeRequest(url, requestComplete);
+
+
 }
 
 window.addEventListener('load', app);
