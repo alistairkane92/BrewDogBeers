5f33f48e2d5c049fd8f8673c1e8e36729351646a
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-06 15:31:27 +0000

    connects to brewdog API

--- public/app.js
+++ public/app.js
@@ -1,5 +1,18 @@
-var app = function(){
+var makeRequest = function(url, callback){
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener("load", callback);
+    request.send();
+};
 
+var requestComplete = function(){
+    if(this.status !== 200) return;
+    console.log("Connected");
+}
+
+var app = function(){
+    var url = "https://api.punkapi.com/v2/beers"
+    makeRequest(url, requestComplete);
 }
 
 window.addEventListener('load', app);
